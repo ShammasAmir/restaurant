11e09bea9cc14e980ce98a2d0a5413fa2a56ec2e
ShammasAmir <amirshammas@gmail.com>
2022-12-04 02:17:06 +0330

    User authentication system created.

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get("/home",[HomeController::class,"index"]);
+
+Route::get("/",[HomeController::class,"index"]);
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
