6c1ffef3e7dfbadbf8d9659376db8d0611d48330
ShammasAmir <amirshammas@gmail.com>
2022-12-09 14:10:10 +0330

    Users are listed in admin panel.

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,63 @@
+<x-app-layout>
+
+</x-app-layout>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Users</title>
+
+    @include('admin.admincss')
+
+</head>
+
+<div class="container-scroller">
+
+    <body>
+
+        @include('admin.navbar')
+
+        <div style="position:relative; top:60px; right:-150px">
+            <table bgcolor="grey" border="3px">
+                <tr>
+                    <th style="padding:30px">Name</th>
+                    <th style="padding:30px">Email</th>
+                    <th style="padding:30px">Action</th>
+                </tr>
+
+                {{-- <tr align="center">
+                    <td>amir</td>
+                    <td>amir@gmail.com</td>
+                    <td><a href="">Delete</a></td>
+                </tr> --}}
+
+                @foreach ($data as $data)
+                    <tr align="center">
+                        <td>{{$data->name}}</td>
+                        <td>{{$data->email}}</td>
+                        {{-- <td><a href="">Delete</a></td> --}}
+
+                        @if($data->usertype=="0")
+                            <td><a href="{{url('/deleteuser',$data->id)}}">Delete</a></td>
+                        @else
+                            <td><a>Not Allowed</a></td>
+                        @endif
+
+                    </tr>
+                @endforeach
+
+
+
+            </table>
+        </div>
+
+        @include('admin.adminscript')
+
+    </body>
+</div>
+
+</html>
