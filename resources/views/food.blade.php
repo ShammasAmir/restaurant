8802c370b9c182e416b798594e1b48752b99e9a1
ShammasAmir <amirshammas@gmail.com>
2022-12-12 02:44:13 +0330

    Food data from database is displayed in frontend menu.

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -14,24 +14,24 @@
         <div class="col-lg-12">
             <div class="owl-menu-item owl-carousel">
 
-                
-                <div class="item">
-                    <div class='card card1'>
-                        <div class="price">
-                            <h6>$14</h6>
-                        </div>
-                        <div class='info'>
-                            <h1 class='title'>Chocolate Cake</h1>
-                            <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii
-                                do eiusmod teme.</p>
-                            <div class="main-text-button">
-                                <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
-                                            class="fa fa-angle-down"></i></a></div>
+                @foreach ($data as $data)
+                    <div class="item">
+                        <div class='card' style="background-image: url('/foodimage/{{$data->image}}');">
+                            <div class="price">
+                                <h6>{{$data->price}}</h6>
+                            </div>
+                            <div class='info'>
+                                <h1 class='title'>{{$data->title}}</h1>
+                                <p class='description'>{{$data->description}}</p>
+                                <div class="main-text-button">
+                                    <div class="scroll-to-section">
+                                        <a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
+                @endforeach
 
             </div>
         </div>
